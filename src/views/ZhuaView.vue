73f3722ab1af0162f9883471d350a18946cf88fe
MixBadGun <1059129006@qq.com>
2024-08-16 21:08:46 +0800

    port and uuid

--- src/views/ZhuaView.vue
+++ src/views/ZhuaView.vue
@@ -2,48 +2,19 @@
 import { ref } from 'vue'
 import CatchBox from '../components/CatchBox.vue'
 
-const data = ref({
-  name: '隬碼面',
-  meta: {
-    get_chip: 2,
-    own_chip: 2,
-    remain_time: '2/2',
-    need_time: '2年'
-  },
-  details: '',
-  catchs: [
-    {
-      info: {
-        description:
-          '小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥，小哥爱吃𰻝𰻝面。最重要的是，两个李白后，\n速度与激情九。\n\n\n\n速度与激情九。哇哈哈哈哈吓得我手抖1:ͤ̀҉̷̸͍̺̟̳͔̞̙̳̳͕͖̬̮̳͂̿͆ͯ̋̒̇ͨ́͋̄̃͌̉̈ͮ̿͟͠  ̷͇͚̝̘̞̯̦̾ͬ̋̌̂͑ͤ̓ͭ̀͒̌̑̒̎͊͆ͬͬ҉̶̴̩̥͎͖̻̜̰̪̙̝̺͕͓̹̱͚̪ͦͣ͐́͆̀̀ͪ̍ͫ͂̇ͬ̑̉̓̍̋ͦ͗̌̌̊͊̚͞.̢͔̮̖̠͇̝̳̪̩̩̥͎͔̞̳̣̻͓̐̊̔́̀͛̎̑͌̓͑̿́̏ͭͫ̀͋͋̐̍ͦͦ̀̕̚ͅͅ0ͤ̀2҉̷̸͍̺̟̳͔̞̙̳͂̿͆ͯ̋̒̇ͨ́͟͠212 ̷͇͚̝̘̞̯̦̾ͬ̋̌̂͑ͤ̓ͭ̀͒̌̑̒̎͊͆ͬͬ҉̶̴̩̥͎͖̻̜̰̪̙̝̺͕͓̹̱͚̪̱ͦͣ͐́͆̀̀ͪ̍ͫ͂̇ͬ̑̉̓̍̋ͦ͗̌̌̊͊̊́̚͞.̢͔̮̖̠͇̝̳̪̩̩̥͎͔̞̳̣̻͓̜͍͍̐̊̔́̀͛̎̑͌̓͑̿́̏ͭͫ̀͋͋̐̍ͦͦ̀̄̕̚ͅͅ ̷̦̖̘̤̇ͣ̿͗͆̓123.̵̥͈̝͚̘̣̘͍̘͎̟̳̺̗̬̰̤̪̮̞̝̯̣̖̂̿ͫͣ̊̔ͯ́̋̍͞͠҉̴̧̲̗̭̼̩͊ͤ͋͐́̋͡ ̡̛̫͈̺̗̗̭̮͎̗̫̫͉͉͇͚͎͓̦̃͛̔͒̒ͥ̇͂̽̌̈̎̀͆͑͆ͨͬ̽͌̍̚͘͘͡ͅ',
-        display_name: '小鿔',
-        color: 'rgb(198, 193, 191)',
-        image: './resource/blank_placeholder.png',
-        level: {
-          display_name: '★★★★★',
-          color: 'rgb(192, 232, 174)'
-        }
-      }
-    },
-    {
-      info: {
-        description:
-          '小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。小哥是小哥，大哥是大哥，大哥不是小哥，小哥不是大哥。',
-        display_name: '小小小小小小小小小小小小哥',
-        color: 'rgb(198, 193, 191)',
-        image: './resource/blank_placeholder.png',
-        level: {
-          display_name: '★★★★★',
-          color: 'rgb(192, 232, 174)'
-        }
-      }
-    }
-  ]
+import axios from 'axios'
+import { useRoute } from 'vue-router'
+
+const data = ref()
+
+const route = useRoute()
+axios.get(`http://localhost:${route.query.port}/data/${route.query.uuid}.json`).then((response) => {
+  data.value = response.data
 })
 </script>
 
 <template>
-  <div class="wrapper">
+  <div class="wrapper" v-if="data">
     <div class="top-title">{{ data.name }} 的一抓</div>
     <div class="side-title">
       本次获得
