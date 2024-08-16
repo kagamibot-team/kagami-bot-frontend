b4536a2dd8bfe0a792cc7cc4a6cd6fa2897e90fc
Passthem <Passthem@163.com>
2024-08-16 20:54:20 +0800

    兼容后端的架构

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import router from './router'
 
 const app = createApp(App)
 
+router.afterEach((to, from) => {
+    // 为后端开发的钩子，方便检测界面是否渲染完成
+    window.loaded_trigger_signal = true;
+});
+
 app.use(router)
 
 app.mount('#app')
