6335aec000cf96d9419bba32dc8a221defa2efc2
MixBadGun <1059129006@qq.com>
2024-09-06 16:31:17 +0800

    new changes

--- src/views/xjshop/BuyView.vue
+++ src/views/xjshop/BuyView.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { load } from '../../common/get_data';
-import QRCode from '../../components/QRCode.vue';
-import { BuyData } from '../../types/xjshop';
+import { computed, ref } from 'vue'
+import { load } from '../../common/get_data'
+import QRCode from '../../components/QRCode.vue'
+import { BuyData } from '../../types/xjshop'
+import { onMounted } from 'vue'
 
 const data = load<BuyData>({
   remain_chips: 2024,
@@ -10,64 +11,59 @@ const data = load<BuyData>({
   time: '20:10:12',
   records: [
     {
-      title: "粑粑小哥",
+      title: '粑粑小哥',
       price: 10000
     },
     {
-      title: "一个名字稍微长一些的测试商品",
+      title: '一个名字稍微长一些的测试商品',
       price: 314159
     }
   ],
   user: {
     name: '小明',
-    uid: 1001,
+    uid: 1001
   }
-});
+})
 
-const formatter = new Intl.NumberFormat();
-const cost = computed(() => formatter.format(
-  data.value.records.map(d => d.price).reduce((a, b) => a + b)
-));
+const formatter = new Intl.NumberFormat()
+const cost = computed(() =>
+  formatter.format(data.value.records.map((d) => d.price).reduce((a, b) => a + b))
+)
 
-const qrcode_data = computed(() => JSON.stringify(data));
+const qrcode_data = ref()
+onMounted(() => {
+  qrcode_data.value = document.getElementById('shop_main')?.textContent
+})
 </script>
 
 <template>
-  <div class="main">
+  <div id="shop_main" class="main">
     <div>小镜的 Shop 销售小票</div>
-    <hr>
+    <hr />
     <div>日期：{{ data.date }}</div>
     <div>时间：{{ data.time }}</div>
     <div>客户：{{ data.user.name }}</div>
     <div>编号：UID_{{ data.user.uid }}</div>
-    <hr>
+    <hr />
     <div>&nbsp;</div>
     <div v-for="(item, index) of data.records" :key="index">
       -&ensp;{{ item.title }}
-      <div class="right">
-        {{ formatter.format(item.price) }}&ensp;薯片
-      </div>
+      <div class="right">{{ formatter.format(item.price) }}&ensp;薯片</div>
       <div>&ensp;</div>
     </div>
-    <hr>
+    <hr />
     <div>&ensp;</div>
     <div>
       应付
-      <div class="right">
-        {{ cost }} 薯片
-      </div>
+      <div class="right">{{ cost }} 薯片</div>
     </div>
     <div>
       实付
-      <div class="right">
-        {{ cost }} 薯片
-      </div>
+      <div class="right">{{ cost }} 薯片</div>
     </div>
     <div>
       余额
-      <div class="right">
-        {{ formatter.format(data.remain_chips) }} 薯片
-      </div>
+      <div class="right">{{ formatter.format(data.remain_chips) }} 薯片</div>
     </div>
     <div class="qrcode">
       <QRCode :data="qrcode_data" />
@@ -87,7 +83,7 @@ const qrcode_data = computed(() => JSON.stringify(data));
     border-top: solid 2px;
   }
 
-  >div {
+  > div {
     margin: 0;
     line-height: 1.25em;
 
